16e305a8410f5608fefda888c447dfcf33e29f4d
EthanHunt-MI <81817578+EthanHunt-MI@users.noreply.github.com>
2021-11-17 14:43:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+# it is a fun hangman game that users can predict each letter every round.
+
+kelime = 'Cristiano'
+liste = []
+for harf in kelime:
+    liste.append('-')
+hangKelime = len(kelime) * '-'
+
+i = 0
+can = 3
+print('Welcome to the hangman game'.center(100,'*'))
+print(f'the letter you have to find has {len(kelime)} letter: {hangKelime} ')
+while i < len(liste):
+    if can > 0:
+        newKelime = kelime.lower()
+        harfTahmin = input('harf tahmin: ')
+        if len(harfTahmin) == 1:
+            if harfTahmin in newKelime:
+
+                indeks = newKelime.index(harfTahmin)
+                liste.pop(indeks)
+                liste.insert(indeks,harfTahmin) == harfTahmin
+                i += 1
+            else:
+                can -= 1
+            print(liste)
+        else:
+            print('please enter one-letter: ')
+    else:
+        print(f'the word you failed to find is {kelime}')
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+while i < len(kelime):
+    harfYasagi = False
+    harf_tahmin = input('harf tahmininiz: ')
+
+    for harf in kelime:
+        if harf_tahmin == harf:
+            harfYasagi = True
+            adam_asmaca_kelimesi += harf
+    i += 1
+    harfYasagi = False
+print(adam_asmaca_kelimesi) 
+'''
